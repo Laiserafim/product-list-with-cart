19d55bac997bd5a820de0500821387226e122adf
lais serafim <laisserafim8@gmail.com>
2024-10-02 15:59:26 -0300

    Refactor: alterando funcionalidades do carrinho

--- src/entities/product.ts
+++ src/entities/product.ts
@@ -17,8 +17,46 @@ export class Product {
     this._imageUrl = imageUrl;
   }
 
+  toHTML() {
+    const productListHTML = document.getElementById("product-list");
+
+    if (!productListHTML) return;
+
+    const productHTML = document.createElement("li");
+    productHTML.id = this._id;
+
+    productHTML.innerHTML = `
+        <div class="rounded-xl overflow-hidden flex flex-col border border-black h-[270px] w-[250px]">
+          <div class="mb-10 relative h-full bg-red-500">
+            <div class="">img</div>
+            <button id="button-add-to-cart" type="button" class="button rounded-lg text-white font-medium text-xs bg-red-400">Add to
+              Cart</button>
+          </div>
+
+          <div class="flex flex-col">
+            <span class="product-category">${this._category}</span>
+            <span class="product-name text-xl font-medium">${this._name}</span>
+            <span class="product-price">$${this._price}</span>
+          </div>
+        </div>
+    `;
+
+    const buttonAddToCartHTML = productHTML.querySelector(
+      "#button-add-to-cart"
+    );
+    buttonAddToCartHTML?.addEventListener("click", () =>
+      this.incrementQuantity()
+    );
+
+    productListHTML.appendChild(productHTML);
+  }
+
   get id() {
     return this._id;
+  }
+
+  get name() {
+    return this._name;
   }
 
   get quantity() {
@@ -36,6 +74,7 @@ export class Product {
   incrementQuantity() {
     this._quantity++;
     this.calculateTotal();
+    // console.log(`quantity:`, this._quantity);
 
     Cart.addToCart(this);
   }
